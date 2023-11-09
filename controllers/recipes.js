2cb93af7b41ee86101f6623d907ca0a1d8c8d40b
Vytautas77 <133365323+Vytautas77@users.noreply.github.com>
2023-11-09 15:33:35 +0200

    added joi comment

--- controllers/recipes.js
+++ controllers/recipes.js
@@ -1,4 +1,6 @@
 const recipeModel = require("../models/recipes");
+const userModel = require("../models/users");
+const adminModel = require("../models/admin");
 
 const ADD_RECIPE = async (req, res) => {
   try {
@@ -68,10 +70,55 @@ const DELETE_RECIPE = async (req, res) => {
   }
 };
 
+const JOI_USER_COMMENT = async (req, res) => {
+  try {
+    const recipe = await recipeModel.findById(req.params.id);
+    if (!recipe) {
+      return res.status(404).json({ message: "Recipe does not exist" });
+    }
+    const userId = req.body.userId;
+    const user = await userModel.findById(userId);
+    if (!user) {
+      return res.status(404).json({ message: "User does not exist" });
+    }
+    await recipeModel
+      .updateOne({ _id: recipe._id }, { $push: { comments: userId } })
+      .exec();
+    return res.status(200).json({ message: "Comment has been purchased" });
+  } catch (err) {
+    console.log("ERROR: ", err);
+    res.status(500).json({ response: "Something went wrong!" });
+  }
+};
+
+const JOI_ADMIN_COMMENT = async (req, res) => {
+  try {
+    const recipe = await recipeModel.findById(req.params.id);
+    if (!recipe) {
+      return res.status(404).json({ message: "Recipe does not exist" });
+    }
+    const adminId = req.body.adminId;
+    console.log(adminId);
+    const admin = await adminModel.findById(adminId);
+    if (!admin) {
+      return res.status(404).json({ message: "User does not exist" });
+    }
+    await recipeModel
+      .updateOne({ _id: recipe._id }, { $push: { comments: adminId } })
+      .exec();
+    return res.status(200).json({ message: "Comment has been purchased" });
+  } catch (err) {
+    console.log("ERROR: ", err);
+    res.status(500).json({ response: "Something went wrong!" });
+  }
+};
+
 module.exports = {
   ADD_RECIPE,
   GET_RECIPES,
   GET_RECIPE_BY_ID,
   UPDATE_RECIPE,
   DELETE_RECIPE,
+  JOI_USER_COMMENT,
+  JOI_ADMIN_COMMENT,
 };
--- routers/recipes.js
+++ routers/recipes.js
@@ -3,11 +3,14 @@ const router = express.Router();
 const validation = require("../middleware/validation");
 const { recipeRegistrationSchema } = require("../validation/RecipeSchema");
 const adminAuth = require("../middleware/adminAuth");
+const auth = require("../middleware/auth");
 const {
   ADD_RECIPE,
   GET_RECIPES,
   GET_RECIPE_BY_ID,
   DELETE_RECIPE,
+  JOI_USER_COMMENT,
+  JOI_ADMIN_COMMENT,
 } = require("../controllers/recipes");
 
 router.post(
@@ -16,6 +19,8 @@ router.post(
   adminAuth,
   ADD_RECIPE
 );
+router.post("/recipes/userComment/:id", auth, JOI_USER_COMMENT);
+router.post("/recipes/adminComment/:id", adminAuth, JOI_ADMIN_COMMENT);
 router.get("/recipes", GET_RECIPES);
 router.get("/recipes/:id", GET_RECIPE_BY_ID);
 router.put(
